d3452d59da3e4ff7a53867c010a4d86499a92bda
gil <lancemaker@gmail.com>
2018-02-09 13:06:04 -0200

    That first Commit
    exercicios

---
+++ aula1/Problema1/problema1/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>//The stdio.h header defines three variable types, several macros, and
+                //various functions for performing input and output.
+#include <stdlib.h> //The stdlib.h header defines four variable types, several macros
+//, and various functions for performing general functions.
+
+main()
+{
+    printf("Técnicas de Programação é Legal Demais!!!");
+}
---
+++ aula1/Problema2/Problema2/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    char caractere;
+    printf("Digite um caractere:");
+    scanf("%c", &caractere);
+    printf("Ola! Codigo: %c",caractere);
+    return 0;
+}
