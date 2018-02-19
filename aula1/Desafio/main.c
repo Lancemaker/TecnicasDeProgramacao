6ef9c4f309fb792dcb26a15d4c6ad8b960032108
gil <lancemaker@gmail.com>
2018-02-19 16:41:43 -0300

    refactor
    refactor going full math to solve this problem.

--- aula1/Desafio/main.c
+++ aula1/Desafio/main.c
@@ -6,6 +6,8 @@ int main()
 {
     int horas1,minutos1;
     int horas2,minutos2;
+    int difH;
+    int difM;
     //recebendo hora e minutos do comeco
     printf("Digite a hora de inicio(sem os minutos formato 24 horas)\n");
     scanf("%d",&horas1);
@@ -16,7 +18,8 @@ int main()
     scanf("%d",&horas2);
     printf("Digite os minutos em que o jogo terminou\n");
     scanf("%d",&minutos2);
-
-    printf("A partida durou %d horas e %d minutos", abs(24*(abs(horas2-horas1)>=22)-abs(horas2-horas1)),abs(minutos2-minutos1));
+    difH=horas2-horas1;
+    difM=minutos2-minutos1;
+    printf("A partida durou %d horas e %d minutos", 24*(difH<0)+difH,(60*(difM<0))+difM);
     return 0;
 }
